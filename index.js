2d15ea358af5cc3c03f879c13ba193aaa049c1cf
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2025-04-28 07:54:48 +0000

    cambios

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-res.send('¡Hola desde la nube con Express!');
+  res.send('¡Hola desde la nube con Express!');
 });
 
-app.listen(port,() =>{
-    HTMLFormControlsCollection.log(`Servidor escuchando en http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
